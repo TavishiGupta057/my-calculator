93d6060d1e68e9d36721e6d219b421c5ff22c243
TavishiGupta057:git config --global user.email <Your Email ID connected to GitHub>
2025-10-06 23:42:12 +0530

    Added and tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -34,3 +34,11 @@ class TestMultiplyDivideWithValidation:
             divide("10", 2)
 
 # TODO: Students will add TestMultiplyDivide class
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+    assert add(-5, 3) == -2
+
+def test_subtract_negative_numbers():
+    assert subtract(-1, -1) == 0
+    assert subtract(-5, -3) == -2
